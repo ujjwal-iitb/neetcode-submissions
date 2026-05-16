f253b7ed14eb2d70746c77dd8f29335509653f61
Ujjwal Krishn Kumar <86978390+ujjwal-iitb@users.noreply.github.com>
2026-05-16 10:53:31 +0530

    Add: partition-equal-subset-sum - submission-1

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool cp(int diff, int index, vector<int>& nums, unordered_map<int, unordered_map<int, int>>& dp){
+        if(index==nums.size()){
+            return diff==0;
+        }
+
+        if(dp[diff].find(index)==dp[diff].end()){
+            int new_diff_a = diff - nums[index];
+            int new_diff_b = diff + nums[index];
+
+            dp[diff][index] = cp(new_diff_a, index + 1, nums, dp) || cp(new_diff_b, index + 1, nums, dp);
+        }
+        return dp[diff][index];
+    }
+
+    bool canPartition(vector<int>& nums) {
+        int t = accumulate(nums.begin(), nums.end(), 0);
+        if (t % 2 == 1) return false;
+        unordered_map<int, unordered_map<int, int>> dp; //{diff, index}--> 0, 1
+
+        return cp(0, 0, nums, dp);
+    }
+};
