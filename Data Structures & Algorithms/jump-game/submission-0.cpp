f57396d986b26e2526641b257b228c7562c0a500
Ujjwal Krishn Kumar <86978390+ujjwal-iitb@users.noreply.github.com>
2026-05-19 15:40:19 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size(), currMin = n-1;
+
+        for(int i = n-1; i>=0; i--){
+            if(nums[i] + i >= currMin){
+                currMin = i;
+            }
+        }
+
+        return currMin==0;
+    }
+};
