27dd00872105e460c6e2803898dc1a8ce218346d
Ujjwal Krishn Kumar <86978390+ujjwal-iitb@users.noreply.github.com>
2026-06-04 16:58:21 +0530

    Add: reconstruct-flight-path - submission-3

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-3.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+
+    void dfs(string curr, unordered_map<string, vector<string>>& graph, unordered_map<string, int>& outDegree, vector<string>& ans){
+        while(outDegree[curr]>0){
+            int i = outDegree[curr] - 1;
+
+            outDegree[curr]--;
+
+            string newNode = graph[curr][i];
+
+            dfs(newNode, graph, outDegree, ans);
+        }
+        ans.push_back(curr);
+    }
+
+
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        unordered_map<string, vector<string>> graph;
+
+        unordered_map<string, int> inDegree, outDegree;
+
+        //unordered_set<string> nodes;
+
+        for(auto edge: tickets){
+            string u = edge[0], v = edge[1];
+
+            graph[u].push_back(v);
+            inDegree[v]++;
+            outDegree[u]++;
+
+            //nodes.add(u);
+            //nodes.add(v);
+        }
+
+        for(auto& nodeEdges: graph){
+            string node = nodeEdges.first;
+            sort(nodeEdges.second.rbegin(), nodeEdges.second.rend());
+            //reverse(nodeEdges.second.begin(), nodeEdges.second.end());
+        }
+
+        vector<string> ans;
+
+        string start = "JFK";
+
+        dfs(start, graph, outDegree, ans);
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
